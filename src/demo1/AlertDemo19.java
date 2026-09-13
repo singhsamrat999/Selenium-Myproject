f2e51491b5441eacb7e7c37352238d9b6fd756b4
singhsamrat999 <ssamrat896@gmail.com>
2026-09-14 00:57:52 +0530

    allertdemo19

---
+++ src/demo1/AlertDemo19.java
@@ -0,0 +1,74 @@
+package demo1;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class AlertDemo19 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\VENOM\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		driver.get("https://demoqa.com/alerts");
+		Thread.sleep(2000);
+
+		// Locate the prompt button
+		WebElement promptBtn = driver.findElement(By.id("promtButton"));
+
+		// Scroll to element to prevent ad overlay blocking
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);", promptBtn);
+		Thread.sleep(1000);
+
+		// Click using JavaScript to avoid ad overlay click interception
+		js.executeScript("arguments[0].click();", promptBtn);
+		Thread.sleep(2000);
+
+		// 1. Fetch Alert Text
+		String actualResult = driver.switchTo().alert().getText();
+		System.out.println("Alert Text: " + actualResult);
+
+		// 2. Compare Expected and Actual Text for Prompt Box
+		String expResult = "Please enter your name";
+		Assert.assertEquals(actualResult, expResult);
+		System.out.println("Alert Text Verified Successfully!");
+		
+		Thread.sleep(2000);
+		driver.switchTo().alert().accept();
+		System.out.println("Accepted");
+		
+		Thread.sleep(4000);
+//		driver.switchTo().alert().dismiss();
+//		System.out.println("Cancelled");
+		
+		// Scroll again before second click to clear any overlapping ad banners
+		js.executeScript("arguments[0].scrollIntoView(true);", promptBtn);
+		Thread.sleep(1000);
+		js.executeScript("arguments[0].click();", promptBtn);
+		
+		// Explicitly wait until alert is actually present on screen
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+		driver.switchTo().alert().dismiss();
+		System.out.println("Cancelled");
+		
+		driver.close();
+		driver.quit();
+	
+	}
+
+}
